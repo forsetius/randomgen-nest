d588626368fbac9924d9b1d931c7ef9a5ef6d459
Marcin Paździora <forseti.pl@gmail.com>
2021-12-05 22:28:57 +0100

    App setup and security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import compression from 'compression';
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableShutdownHooks();
+  app.enableCors();
+  app.use(compression());
+  app.use(helmet());
+  app.use(RateLimit({
+    max: 10,
+    windowMs: 1000,
+  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: process.env['NODE_ENV']?.startsWith('prod') ?? false,
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(process.env['PORT'] ?? 3000);
 }
+
 void bootstrap();
