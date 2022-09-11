77cfa3fc8bd27c76c05e97112f546f034df62ba7
Marcin Paździora <forseti.pl@gmail.com>
2022-09-11 19:56:12 +0200

    Rm artifacts

--- src/knk/renderers/JsonKnkRenderer.ts
+++
@@ -1,8 +0,0 @@
-import { RendererInterface } from '../../app/render/RendererInterface';
-import { Faction } from '../domain/Faction';
-
-export class JsonKnkRenderer implements RendererInterface {
-  public render(data: Faction[]) {
-
-  }
-}
